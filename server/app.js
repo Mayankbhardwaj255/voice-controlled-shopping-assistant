098a3c2b202330bcd8f9b699cbc10cdf40ea8f77
Mayankbhardwaj255 <mayankbhardwaj708@gmail.com>
2025-08-31 23:13:26 +0530

    update app.js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+// server/index.js
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -6,24 +7,24 @@ const dotenv = require('dotenv');
 const voiceRoute = require('./routes/voiceRoute');
 
 dotenv.config();
-// console.log("🔑 Loaded API key:", process.env.GEMINI_API_KEY?.slice(0, 8));
+
 const app = express();
 const __dirname = path.resolve();
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 
-
+// API route
 app.use('/voice-command', voiceRoute);
-if(process.env.NODE_ENV === "production" ){
-    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "dist"))); // frontend build folder
 
-})
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "dist", "index.html"));
+    });
 }
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
