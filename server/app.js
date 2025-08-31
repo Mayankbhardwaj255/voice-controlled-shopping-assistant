2ff3f3e9ab2da93669743a6ce5f4ca51e78e2657
Mayank Bhardwaj <mayankbhardwaj708@gmail.com>
2025-09-01 01:57:06 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use('/voice-command', voiceRoute);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "dist"))); // frontend build folder
 
-    app.get("/*", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "dist", "index.html"));
     });
 }
